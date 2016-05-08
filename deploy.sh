9d3c255b1a2d895881d84e4dbd7e8a9bd8500ed4
vagrant <vagrant@localhost.localdomain>
2016-05-09 01:03:41 +0900

    deploy.sh 修正

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,22 @@ export DOTPATH
 
 cd $DOTPATH
 
-for dotfile in `ls .??*`
+for dotfile in .??*
 do
-    [ "$dotfile" = ".git" ] && continue
-    [ "$dotfile" = ".DS_Store" ] && continue
+  [ "$dotfile" = ".git" ] && continue
+  [ "$dotfile" = ".DS_Store" ] && continue
 
-    echo
-    echo "loading $dotfile ..."
-    if [ -e "$HOME"/"$dotfile" ] ; then
-      /bin/echo -n "overwrite ~/$dotfile ? (yn) > "
-      read yn
-      if [ ${yn:-"no"} = "yes" ] || [ ${yn:-"no"} = "y" ] ; then
-        ln -snfv "$DOTPATH"/"$dotfile" "$HOME"/"$dotfile"
-      fi
-    fi
+  echo
+  echo "loading $dotfile ..."
+  if [ -e "$HOME"/"$dotfile" ] ; then
+    /bin/echo -n "overwrite ~/$dotfile ? (yn) > "
+    read yn
+  else
+    yn="y"
+  fi
+  if [ ${yn:-"no"} = "yes" ] || [ ${yn:-"no"} = "y" ] ; then
+    ln -snfv "$DOTPATH"/"$dotfile" "$HOME"/"$dotfile"
+  fi
 done
 
 echo
