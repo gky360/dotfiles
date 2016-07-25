e304ae37abe3188ec60e8db2f818d68913724120
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-07-26 01:31:02 +0900

    command_exists の変数指定の修正

--- _zshrc/01_paths.sh
+++ _zshrc/01_paths.sh
@@ -6,21 +6,21 @@ export PATH
 
 # ruby, rails
 export PATH="$HOME/.rbenv/bin:$PATH"
-if command_exists rbenv ; then
+if command_exists 'rbenv' ; then
   eval "$(rbenv init --no-rehash -)"
 fi
 
 # python
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-if command_exists pyenv ; then
+if command_exists 'pyenv' ; then
   eval "$(pyenv init -)"
 fi
 
 # go 言語
 # [Macでgo言語開発環境を作る]
 # (http://qiita.com/puttyo_bubu/items/4e60e42ff041f2474428)
-if command_exists go ; then
+if command_exists 'go' ; then
   if [ -x "`which go`" ]; then
     export GOROOT=`go env GOROOT`
     export GOPATH=$HOME/go
@@ -29,22 +29,22 @@ if command_exists go ; then
 fi
 
 # Android
-if command_exists android ; then
+if command_exists 'android' ; then
   export ANDROID_HOME=/usr/local/opt/android-sdk
 fi
 
 # cordova
-if command_exists cordova ; then
+if command_exists 'cordova' ; then
   export PATH=$PATH:/usr/local/Cellar/android-sdk/24.4/tools
   export PATH=$PATH:/usr/local/Cellar/android-sdk/24.4/platform-tools
 fi
 
 # postgreSQL
-if command_exists postgres ; then
+if command_exists 'postgres' ; then
   export PGDATA=/usr/local/var/postgres
 fi
 
 # node
-# export NVM_DIR="/Users/inagaki/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+export NVM_DIR="/Users/inagaki/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
