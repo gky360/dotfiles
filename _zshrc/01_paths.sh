ebef4e9cfb0831ecbb6c2f70e87274e9605eb3e7
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-08-15 23:50:28 +0900

    いらない path 削除

--- _zshrc/01_paths.sh
+++ _zshrc/01_paths.sh
@@ -17,28 +17,6 @@ if command_exists 'pyenv' ; then
   eval "$(pyenv init -)"
 fi
 
-# go 言語
-# [Macでgo言語開発環境を作る]
-# (http://qiita.com/puttyo_bubu/items/4e60e42ff041f2474428)
-if command_exists 'go' ; then
-  if [ -x "`which go`" ]; then
-    export GOROOT=`go env GOROOT`
-    export GOPATH=$HOME/go
-    export PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-  fi
-fi
-
-# Android
-if command_exists 'android' ; then
-  export ANDROID_HOME=/usr/local/opt/android-sdk
-fi
-
-# cordova
-if command_exists 'cordova' ; then
-  export PATH=$PATH:/usr/local/Cellar/android-sdk/24.4/tools
-  export PATH=$PATH:/usr/local/Cellar/android-sdk/24.4/platform-tools
-fi
-
 # postgreSQL
 if command_exists 'postgres' ; then
   export PGDATA=/usr/local/var/postgres
