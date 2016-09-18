74c7b9c731084b57643de32940430599f724733b
vagrant <vagrant@localhost.localdomain>
2016-09-18 07:53:32 +0000

    nvm の読み込みを修正

--- _zshrc/01_paths.sh
+++ _zshrc/01_paths.sh
@@ -32,16 +32,18 @@ fi
 
 # nvm
 # 参考: [NVM の nvm.sh を遅延ロードしてシェルの起動を高速化する - Qiita](http://qiita.com/uasi/items/80865646607b966aedc8)
-export NVM_DIR="$HOME/.nvm"
-NVM_DEFAULT_VERSION=`cat $NVM_DIR/alias/default`
-NVM_DEFAULT_DIR=$NVM_DIR/versions/node/v6.4.0
-PATH=$NVM_DEFAULT_DIR/bin:$PATH
-MANPATH=$NVM_DEFAULT_DIR/share/man:$MANPATH
-export NODE_PATH=$NVM_DEFAULT_DIR/lib/node_modules
-NODE_PATH=${NODE_PATH:A}
-nvm() {
-  unset -f nvm
-  source "$NVM_DIR/nvm.sh"
-  nvm "$@"
-}
+if [ -s "$HOME/.nvm" ]; then
+  export NVM_DIR="$HOME/.nvm"
+  NVM_DEFAULT_VERSION=`cat $NVM_DIR/alias/default`
+  NVM_DEFAULT_DIR=$NVM_DIR/versions/node/v6.4.0
+  PATH=$NVM_DEFAULT_DIR/bin:$PATH
+  MANPATH=$NVM_DEFAULT_DIR/share/man:$MANPATH
+  export NODE_PATH=$NVM_DEFAULT_DIR/lib/node_modules
+  NODE_PATH=${NODE_PATH:A}
+  nvm() {
+    unset -f nvm
+    source "$NVM_DIR/nvm.sh"
+    nvm "$@"
+  }
+fi
 
