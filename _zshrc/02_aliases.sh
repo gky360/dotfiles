989ff38bc515ff019f8a6691321ffa40f268fca9
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-12-17 02:38:56 +0900

    fix rails aliases

--- _zshrc/02_aliases.sh
+++ _zshrc/02_aliases.sh
@@ -8,7 +8,8 @@ alias be='bundle exec'
 alias rsb='bundle exec rails s -b 0.0.0.0'
 alias rtcc='bundle exec rake tmp:cache:clear'
 alias Prc='bundle exec rails c -e production'
-alias Prsb='RAILS_ENV=production bundle exec rake assets:precompile assets:clean && bundle exec rails s -b 0.0.0.0 -e production'
+alias Prap'RAILS_ENV=production bundle exec rake assets:precompile assets:clean'
+alias Prsb='bundle exec rails s -b 0.0.0.0 -e production'
 
 # git
 alias gbrcl="git branch --merged | grep -v '*' | xargs git branch -d"
