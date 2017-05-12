8cff2c2ab8a0933377af13bc4427a388f1ac9864
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-05-12 13:48:15 +0900

    fix emacs config

--- .emacs.el
+++ .emacs.el
@@ -33,3 +33,32 @@
         (if (not (and (boundp 'mule-x-win-initted) mule-x-win-initted))
             (require 'sym-lock))
         (require 'font-lock)))
+
+
+(set-default-coding-systems 'utf-8-unix)    ; UTF-8 が基本
+(set-terminal-coding-system 'utf-8-unix)    ; emacs -nw も文字化けしない
+(setq default-file-name-coding-system 'utf-8)
+(setq default-process-coding-system '(utf-8 . utf-8))
+(prefer-coding-system 'utf-8-unix)
+
+;; ロックファイル / バックアップファイルを作成しない
+(setq create-lockfiles nil)
+(setq make-backup-files nil)
+(setq delete-auto-save-files t)
+
+;; 対応するカッコを強調表示
+(show-paren-mode t)
+
+;; 時間も表示させる。
+(display-time)
+
+;; 行番号を常に表示させる
+(global-linum-mode)
+(setq linum-format "%4d ")
+
+;; 現在行を目立たせる
+(setq hl-line-face 'underline)
+(global-hl-line-mode)
+
+;; テーマを設定する
+(load-theme 'manoj-dark t)
