e8eca49e3582d28ba7da65d49c7a614e758afe3f
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-07-08 15:41:44 +0900

    fix g++ alias

--- _x_shrc/00_funcs.sh
+++ _x_shrc/00_funcs.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 function command_exists () {
-    command -v "$1" >/dev/null 2>&1 ;
+  command -v "$1" >/dev/null 2>&1 ;
 }
 
 function gpwo() {
-    g++ -Wall -std=c++14 ${1%.cpp}.cpp -o ${1%.cpp}.out
+  set +x
+  g++ -Wall -std=c++14 -o ${1%.cpp}.out ${@:2} ${1%.cpp}.cpp
 }
 
 
