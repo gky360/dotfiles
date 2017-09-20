2a13f06f15f0fea1b1ddb3e93c260a4a2e04ea3e
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-09-21 00:00:34 +0900

    add tmux install script

---
+++ scripts/tmux_install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+libevent_version=2.1.8
+tmux_version=2.5
+working_dir=$HOME/tmp
+target_dir=$HOME/.local
+
+mkdir -p $working_dir
+
+# libevent
+cd $working_dir
+wget https://github.com/libevent/libevent/releases/download/release-${libevent_version}-stable/libevent-${libevent_version}-stable.tar.gz
+tar xvf libevent-${libevent_version}-stable.tar.gz
+cd libevent-${libevent_version}-stable
+./configure --prefix=$target_dir
+make # use make -j 8 to speed it up if your machine is capable
+make install
+
+# tmux
+cd $working_dir
+wget https://github.com/tmux/tmux/releases/download/${tmux_version}/tmux-${tmux_version}.tar.gz
+tar xvf tmux-${tmux_version}.tar.gz
+cd tmux-${tmux_version}
+./configure --prefix=$target_dir CFLAGS="-I$working_dir/include" LDFLAGS="-L$working_dir/lib"
+make
+make install
+
+echo
+echo
+echo 'Add \"export PATH=$HOME/.local/bin:$PATH\" to your .bashrc or .zshrc .'
+
