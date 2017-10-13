28720716af3a893e9708682718f8a8d52e562238
scarletrunner7000 <scarletrunner7000@gmail.com>
2017-10-13 17:22:43 +0900

    fix tmux install script

--- scripts/tmux_install.sh
+++ scripts/tmux_install.sh
@@ -11,16 +11,20 @@ mkdir -p $working_dir
 
 # libevent
 cd $working_dir
-wget -c https://github.com/libevent/libevent/releases/download/release-${libevent_version}-stable/libevent-${libevent_version}-stable.tar.gz
-tar xvf libevent-${libevent_version}-stable.tar.gz
-cd libevent-${libevent_version}-stable
-./configure --prefix=$target_dir
-make # use make -j 8 to speed it up if your machine is capable
-make install
+if ! [ -e ${target_dir}/lib/libevent.so ]; then
+  wget -c https://github.com/libevent/libevent/releases/download/release-${libevent_version}-stable/libevent-${libevent_version}-stable.tar.gz\
+    -O libevent-${libevent_version}-stable.tar.gz
+  tar xvf libevent-${libevent_version}-stable.tar.gz
+  cd libevent-${libevent_version}-stable
+  ./configure --prefix=$target_dir
+  make # use make -j 8 to speed it up if your machine is capable
+  make install
+fi
 
 # tmux
 cd $working_dir
-wget -c https://github.com/tmux/tmux/releases/download/${tmux_version}/tmux-${tmux_version}.tar.gz
+wget -c https://github.com/tmux/tmux/releases/download/${tmux_version}/tmux-${tmux_version}.tar.gz\
+  -O tmux-${tmux_version}.tar.gz
 tar xvf tmux-${tmux_version}.tar.gz
 cd tmux-${tmux_version}
 ./configure --prefix=$target_dir CFLAGS="-I${target_dir}/include" LDFLAGS="-L${target_dir}/lib"
