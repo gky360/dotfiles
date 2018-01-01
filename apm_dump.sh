ccdffd36bd5551d23a9dd01407918294259ff3a0
gky360 <gky360@gmail.com>
2018-01-01 23:08:24 +0900

    add atom packages

--- apm_dump.sh
+++ apm_dump.sh
@@ -7,4 +7,4 @@ export DOTPATH
 
 cd $DOTPATH
 
-apm list --installed --bare | awk '{ print tolower($0) }' | tee ${DOTPATH}/_atom/packages-list.txt
+apm list --installed --bare | awk '{ print tolower($0) }' | sed '/^\s*$/d' | sort | tee ${DOTPATH}/_atom/packages-list.txt
