ed386a87a727d3583994617224f41d0ac22f4cba
gky360 <gky360@gmail.com>
2018-01-12 09:13:46 +0900

    use mactex instead of basictex

--- _x_shrc/01_paths.sh
+++ _x_shrc/01_paths.sh
@@ -37,7 +37,7 @@ if command_exists 'postgres' ; then
 fi
 
 # latex
-TEXLIVE_BIN=/usr/local/texlive/2017basic/bin/x86_64-darwin
+TEXLIVE_BIN=/usr/local/texlive/2017/bin/x86_64-darwin
 if [ -e $TEXLIVE_BIN ]; then
   export PATH=$PATH:$TEXLIVE_BIN
 fi
