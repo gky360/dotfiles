1f331e9bd9ebe5c3e19b097d64c9c3fe10d49f38
gky360 <gky360@gmail.com>
2018-04-02 20:39:41 +0900

    fix ansible init script

--- ansible/init.sh
+++ ansible/init.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-sudo xcodebuild -license
-xcode-select --install
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
-
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew doctor
