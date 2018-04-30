3711eacf5e18aff71ae78edccafaf01243a992e6
gky360 <gky360@gmail.com>
2018-05-01 02:14:45 +0900

    add url2md.rb

---
+++ scripts/url2md.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+require 'open-uri'
+require 'open_uri_redirections'
+require 'uri'
+
+url = `pbpaste`
+doc = Nokogiri::HTML(open(url, :allow_redirections => :all))
+title = doc.title.strip.gsub(/[\s]*(\r\n?|\n)[\s]*/," ")
+md_link_text = "[#{title}](#{URI::unescape(url)})"
+puts md_link_text
+`/bin/echo -n '#{md_link_text}' | pbcopy`
