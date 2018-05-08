772f006cf416e2e4893632f32bc193f72119bf36
gky360 <gky360@gmail.com>
2018-05-08 14:11:22 +0900

    fix brew bundle dump script

--- scripts/brew_dump.sh
+++ scripts/brew_dump.sh
@@ -8,4 +8,11 @@ export DOTPATH
 cd $DOTPATH
 cd ansible
 
-brew bundle dump --force
+brew bundle dump --describe --force
+mv Brewfile Brewfile.tmp
+cat <<EOF | cat - Brewfile.tmp > Brewfile
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+EOF
+rm -f Brewfile.tmp
