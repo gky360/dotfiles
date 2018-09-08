8ca6efcf620f78e6f8330fee8dcc9e430e02e566
gky360 <gky360@gmail.com>
2018-09-08 23:45:05 +0900

    fix go install script

--- scripts/go_install.sh
+++ scripts/go_install.sh
@@ -2,25 +2,13 @@
 
 set -e
 
-GO_VERSION=1.10
+GO_VERSION=1.11
 
 working_dir=$HOME
 target_dir=$HOME/.local
 mkdir -p $working_dir
-mkdir -p $target_dir/bin
+mkdir -p $target_dir
 
-
-function install_go() {
-  cd $working_dir
-  if ! [ -d go${1} ]; then
-    git clone https://go.googlesource.com/go go${1}
-  fi
-  cd go${1}/src
-  git checkout release-branch.go${1}
-  ./make.bash
-}
-
-install_go 1.4
-install_go $GO_VERSION
-
-ln -sf ${working_dir}/go${GO_VERSION}/bin/* $target_dir/bin
+cd $working_dir
+wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
+tar -C $target_dir -xzf go$GO_VERSION.linux-amd64.tar.gz
