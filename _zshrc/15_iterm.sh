89037d6d6d86cba3a49abcb09eb3df70171f88d3
gky360 <gky360@gmail.com>
2018-10-17 04:34:59 +0900

    unset title of iterm tabs when exiting ssh

--- _zshrc/15_iterm.sh
+++ _zshrc/15_iterm.sh
@@ -8,56 +8,64 @@ fi
 # iTerm2 window/tab color commands
 #   Requires iTerm2 >= Build 1.0.0.20110804
 #   http://code.google.com/p/iterm2/wiki/ProprietaryEscapeCodes
-tab-color-rgb() {
-echo -ne "\033]6;1;bg;red;brightness;$1\a"
-echo -ne "\033]6;1;bg;green;brightness;$2\a"
-echo -ne "\033]6;1;bg;blue;brightness;$3\a"
+tab_color_rgb() {
+  echo -ne "\033]6;1;bg;red;brightness;$1\a"
+  echo -ne "\033]6;1;bg;green;brightness;$2\a"
+  echo -ne "\033]6;1;bg;blue;brightness;$3\a"
 }
-tab-color-reset() {
-echo -ne "\033]6;1;bg;*;default\a"
+tab_color_reset() {
+  echo -ne "\033]6;1;bg;*;default\a"
 }
-tab-color() {
-case $1 in
-  black)
-    tab-color-rgb 0 0 0
-    ;;
-  red)
-    tab-color-rgb 170 51 51
-    ;;
-  green)
-    tab-color-rgb 51 170 51
-    ;;
-  yellow)
-    tab-color-rgb 170 170 51
-    ;;
-  blue)
-    tab-color-rgb 51 51 170
-    ;;
-  magenta)
-    tab-color-rgb 170 51 170
-    ;;
-  cyan)
-    tab-color-rgb 51 170 170
-    ;;
-  white)
-    tab-color-reset
-    ;;
-  *)
-    echo invalid color name >&2
-    ;;
+tab_color() {
+  case $1 in
+    black)
+      tab_color_rgb 0 0 0
+      ;;
+    red)
+      tab_color_rgb 170 51 51
+      ;;
+    green)
+      tab_color_rgb 51 170 51
+      ;;
+    yellow)
+      tab_color_rgb 170 170 51
+      ;;
+    blue)
+      tab_color_rgb 51 51 170
+      ;;
+    magenta)
+      tab_color_rgb 170 51 170
+      ;;
+    cyan)
+      tab_color_rgb 51 170 170
+      ;;
+    white)
+      tab_color_reset
+      ;;
+    *)
+      echo invalid color name >&2
+      ;;
   esac
-
 }
+
+tab_title() {
+  echo -ne "\033]0;$1\007"
+}
+tab_title ""
 
 # Change the color of the tab when using SSH
 # reset the color after the connection closes
-color-ssh() {
-if [[ -n "$ITERM_SESSION_ID" ]]; then
-  trap "tab-color-reset" INT EXIT
-  tab-color ${zshrc_prompt_color:-'white'}
-fi
-ssh $*
+finish_ssh() {
+  tab_color_reset
+  tab_title ""
 }
-compdef _ssh color-ssh=ssh
+color_ssh() {
+  if [[ -n "$ITERM_SESSION_ID" ]]; then
+    trap "finish_ssh" INT EXIT
+    tab_color ${theme_color:-'white'}
+  fi
+  \ssh $*
+}
+compdef _ssh color_ssh=ssh
 
-alias ssh='color-ssh '
+alias ssh='color_ssh '
