75bf471c7028e75e46779bb5e52df885da1a45ae
gky360 <gky360@gmail.com>
2018-12-19 14:58:43 +0900

    [sh] fix git alias

--- _x_shrc/02_aliases.sh
+++ _x_shrc/02_aliases.sh
@@ -33,6 +33,8 @@ alias Prsb='bundle exec rails s -b 0.0.0.0 -e production'
 alias gst="git status"
 ## log graph
 alias ggr="git gr"
+## status and log graph
+alias gsr="gst && echo && ggr"
 ## clear unused branches
 alias gbrcl="git branch --merged | grep -v '*' | xargs git branch -d"
 ## reset modes of the files
