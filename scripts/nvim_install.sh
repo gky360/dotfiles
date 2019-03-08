55030a5f05c81e38b60c7c5983274693ca8c560d
gky360 <gky360@gmail.com>
2019-03-08 16:00:46 +0900

    fix nvim install script

--- scripts/nvim_install.sh
+++ scripts/nvim_install.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-set -ex
+set -eux
+
+WORKING_DIR=$HOME/tmp
+TARGET_DIR=$HOME/.local
+
+if [ -x "$(command -v nvim)" ]; then
+  cd $WORKING_DIR
+  git clone https://github.com/neovim/neovim
+  make CMAKE_BUILD_TYPE=Release CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$TARGET_DIR"
+  make install
+fi
+
+
+# Support Python3 provider
 
 PYTHON_VERSION=3.7.2
 
@@ -8,4 +21,5 @@ pyenv install $PYTHON_VERSION || true
 pyenv virtualenv $PYTHON_VERSION nvim3
 . ~/.bashrc
 pyenv activate nvim3
+set +u
 pip install pynvim
