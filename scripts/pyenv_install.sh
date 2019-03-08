8b216e64cdefc0ff94d32dc6e7d0c20ce6bf2db4
gky360 <gky360@gmail.com>
2019-03-08 18:40:39 +0900

    fix pyenv install script

--- scripts/pyenv_install.sh
+++ scripts/pyenv_install.sh
@@ -5,7 +5,11 @@ set -eux
 if ! [ -x "$(command -v pyenv)" ]; then
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 fi
+cd $(pyenv root)
+git pull
 
 if ! [ -e $(pyenv root)/plugins/pyenv-virtualenv ]; then
   git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
 fi
+cd $(pyenv root)/plugins/pyenv-virtualenv
+git pull
