96bf6ff83d74613cc36bf03318c705fae4d61956
gky360 <gky360@gmail.com>
2019-04-08 03:58:31 +0900

    [nvim] fix install script

--- scripts/nvim_install.sh
+++ scripts/nvim_install.sh
@@ -12,7 +12,7 @@ PYTHON_VERSION=3.7.2
 $DOTPATH/scripts/pyenv_install.sh
 
 pyenv install $PYTHON_VERSION || true
-if ! [ -e $(pyenv root)/versions/3.7.2/envs/nvim3 ]; then
+if ! [ -e $(pyenv root)/versions/$PYTHON_VERSION/envs/nvim3 ]; then
   pyenv virtualenv $PYTHON_VERSION nvim3
 fi
 set +u
@@ -22,8 +22,18 @@ pyenv activate nvim3
 pyenv which python
 pip install pynvim
 
+# Install dependencies
+if [ -f /etc/lsb-release ]; then
+  # Debian/Ubuntu
+  sudo apt-get update
+  sudo apt-get -y install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip
+elif [ -f /etc/redhat-release ]; then
+  # CentOS
+  sudo yum -y install ninja-build libtool autoconf automake cmake gcc gcc-c++ make pkgconfig unzip
+fi
 
 if ! [ -x "$(command -v nvim)" ]; then
+  mkdir -p $WORKING_DIR
   cd $WORKING_DIR
   if ! [ -e neovim ]; then
     git clone https://github.com/neovim/neovim
