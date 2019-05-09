77512d20e1ab1882f7a3fede5d7dffc63bf9fa8f
gky360 <gky360@gmail.com>
2019-05-10 03:02:06 +0900

    add scripts for windows

---
+++ scripts/choco.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eux
+
+choco upgrade \
+  anaconda3 \
+  cmake \
+  cmder \
+  docker-toolbox \
+  git \
+  golang \
+  googlechrome \
+  make \
+  myrica \
+  nvm \
+  slack \
+  sourcetree \
+  vagrant \
+  vim \
+  virtualbox \
+  vscode \
+  wget \
+  -y
