134055b369245af8ce9cbaeb87c3aa04a30e932f
gky360 <gky360@gmail.com>
2019-12-03 12:20:57 +0900

    [zsh] fix prompt

--- _zshrc/14_sukoshikotta.sh
+++ _zshrc/14_sukoshikotta.sh
@@ -21,7 +21,7 @@ SAVEHIST=1000000
 # 1行表示
 # PROMPT="%~ %# "
 # 2行表示
-PROMPT="%{${fg[$zshrc_prompt_color]}%}[%n@%m]%{${reset_color}%} %~
+PROMPT="%{${fg[$zshrc_prompt_color]}%}%n@%M:%{${reset_color}%}%~
 %{%(?.$fg[white].$fg[red])%}%#%{$reset_color%} "
 
 
