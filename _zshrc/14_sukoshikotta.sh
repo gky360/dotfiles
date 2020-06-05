fa07bec09265d7b38085aac1298a0d6112b1463f
gky360 <gky360@gmail.com>
2020-06-05 21:58:29 +0900

    [zsh] Fix completion

--- _zshrc/14_sukoshikotta.sh
+++ _zshrc/14_sukoshikotta.sh
@@ -52,6 +52,26 @@ zstyle ':completion:*:sudo:*' command-path /usr/local/sbin /usr/local/bin \
 # ps コマンドのプロセス名補完
 zstyle ':completion:*:processes' command 'ps x -o pid,s,args'
 
+zstyle ':completion:*' menu select interactive
+setopt menu_complete
+
+zmodload zsh/complist                                         # "bindkey -M menuselect"設定できるようにするためのモジュールロード
+bindkey -v '^a' beginning-of-line                             # 行頭へ(menuselectでは補完候補の先頭へ)
+bindkey -v '^b' backward-char                                 # 1文字左へ(menuselectでは補完候補1つ左へ)
+bindkey -v '^e' end-of-line                                   # 行末へ(menuselectでは補完候補の最後尾へ)
+bindkey -v '^f' forward-char                                  # 1文字右へ(menuselectでは補完候補1つ右へ)
+bindkey -v '^h' backward-delete-char                          # 1文字削除(menuselectでは絞り込みの1文字削除)
+bindkey -v '^i' expand-or-complete                            # 補完開始
+bindkey '^p' history-beginning-search-backward
+bindkey '^n' history-beginning-search-forward
+bindkey -M menuselect '^g' .send-break                        # send-break2回分の効果
+bindkey -M menuselect '^i' forward-char                       # 補完候補1つ右へ
+bindkey -M menuselect '^j' .accept-line                       # accept-line2回分の効果
+bindkey -M menuselect '^k' accept-and-infer-next-history      # 次の補完メニューを表示する
+bindkey -M menuselect '^n' down-line-or-history               # 補完候補1つ下へ
+bindkey -M menuselect '^p' up-line-or-history                 # 補完候補1つ上へ
+bindkey -M menuselect '^r' history-incremental-search-forward # 補完候補内インクリメンタルサーチ
+
 ########################################
 # vcs_info
 autoload -Uz vcs_info
