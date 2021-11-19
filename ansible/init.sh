87d37ec9c681e254fa44284a23010919eef1ea0b
gky360 <gky360@gmail.com>
2021-11-19 17:59:16 +0900

    Install rosetta if M1

--- ansible/init.sh
+++ ansible/init.sh
@@ -4,11 +4,15 @@ set -e
 
 xcode-select --install
 
+if [[ $(uname -p) == 'arm' ]]; then
+  # has M1 chip
+  softwareupdate --install-rosetta
+fi
+
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 brew doctor
 brew update
 
-brew install python
 brew install ansible
 brew install git
