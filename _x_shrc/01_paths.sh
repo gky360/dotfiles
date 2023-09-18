f65b0fe3d8ec02e753af381914c24f15bef734c6
gky360 <gky360@gmail.com>
2023-09-18 16:47:00 +0900

    [bash] Update goenv

--- _x_shrc/01_paths.sh
+++ _x_shrc/01_paths.sh
@@ -69,9 +69,9 @@ fi
 export RUST_BACKTRACE=1
 
 # golang
+export GOENV_ROOT="$HOME/.goenv"
+export PATH="$GOENV_ROOT/bin:$PATH"
 if command_exists 'goenv' ; then
-  export GOENV_ROOT="$HOME/.goenv"
-  export PATH="$GOENV_ROOT/bin:$PATH"
   eval "$(goenv init -)"
   export PATH="$GOROOT/bin:$PATH"
   export PATH="$PATH:$GOPATH/bin"
